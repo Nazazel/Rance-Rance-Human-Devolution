e410e377ac366713d5b868d434dec3779c88ff18
TheAvianLord <angela-yixuan21@hotmail.com>
2017-05-02 20:49:45 -0700

    Song Scenes will go back to Second Menu

--- Rance Rance Human Devolution/Assets/Scripts/Play.cs
+++ Rance Rance Human Devolution/Assets/Scripts/Play.cs
@@ -1,19 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Play : MonoBehaviour {
-	public AudioSource audio;
+public class Play : MonoBehaviour
+{
+    public AudioSource audio;
+    public bool started;
 
-	// Use this for initialization
-	void Start () {
-		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
-		audio = gameObject.GetComponent<AudioSource> ();
-		audio.Play ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+        audio = gameObject.GetComponent<AudioSource>();
+        audio.Play();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audio.isPlaying)
+        {
+            if (!started)
+            {
+                started = true;
+                StartCoroutine("wait");
+            }
+        }
+        
+    }
+    IEnumerator wait()
+    {
+        started = false;
+        yield return new WaitForSeconds(5F);
+        SceneManager.LoadSceneAsync("Second Menu");
+    }
 }
