41378de84ff2030d988dc0081a5705c47e5375ce
Nazely Hartoonian <nhartoon@uci.edu>
2017-05-04 16:04:39 -0700

    added new background image to credits page
    credits sizing and location need to be changed + credits need to be
    accessed via menu

--- Rance Rance Human Devolution/Assets/Scripts/Credits.cs
+++ Rance Rance Human Devolution/Assets/Scripts/Credits.cs
@@ -42,6 +42,6 @@ public class Credits : MonoBehaviour
         theText.text = " ";
         //StartCoroutine(AudioFadeOut.FadeOut(GameObject.Find("Music").GetComponent<AudioSource>(), 2.5f));
         //yield return new WaitForSeconds(3.0f);
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("NewMenu");
     }
 }
