ec2517af4a3a053fb3d2488aa9e14888ed153804
CodiakVII <codiak759@gmail.com>
2017-05-09 21:08:50 -0700

    Scene Changes

--- Rance Rance Human Devolution/Assets/Scripts/Credits.cs
+++ Rance Rance Human Devolution/Assets/Scripts/Credits.cs
@@ -7,12 +7,14 @@ using UnityEngine.UI;
 public class Credits : MonoBehaviour
 {
     public bool started = false;
+	public float fadeSpeed = 1.5f;
     public Text theText;
+	public Image FadeScreen;
 
     // Use this for initialization
     void Start()
     {
-
+		FadeScreen = GameObject.Find ("Fade").GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -27,6 +29,7 @@ public class Credits : MonoBehaviour
 
     IEnumerator waitThreeSeconds()
     {
+		InvokeRepeating ("FadeToClear", 0.0f, 0.1f);
         yield return new WaitForSeconds(3.0f);
 		GameObject.Find ("Audio Source").GetComponent<AudioSource> ().Play ();
         theText.text = "Project Leads:\n\nNazely  Hartoonian\n\nUlises Perez";
@@ -40,8 +43,34 @@ public class Credits : MonoBehaviour
         theText.text = "Design:\n\nUlises Perez\n\n Nazely  Hartoonian";
         yield return new WaitForSeconds(3.0f);
         theText.text = " ";
-        //StartCoroutine(AudioFadeOut.FadeOut(GameObject.Find("Music").GetComponent<AudioSource>(), 2.5f));
-        //yield return new WaitForSeconds(3.0f);
+        StartCoroutine(AudioFadeOut.FadeOut(GameObject.Find("Audio Source").GetComponent<AudioSource>(), 2.5f));
+        yield return new WaitForSeconds(3.0f);
         SceneManager.LoadScene("NewMenu");
     }
+
+	public void FadeToBlack()
+	{
+		FadeScreen.color = Color.Lerp (FadeScreen.color, Color.black, fadeSpeed * Time.deltaTime);
+		if (FadeScreen.color.a == 1.0f) {
+			CancelInvoke ("FadeToBlack");
+		}
+	}
+
+	public void FadeToClear()
+	{
+		if (SceneManager.GetActiveScene ().name == "Lose") {
+			GameObject.Find ("Button").GetComponent<Image> ().enabled = false;
+		}
+		FadeScreen.color = Color.Lerp (FadeScreen.color, Color.clear, fadeSpeed * Time.deltaTime);
+		if (FadeScreen.color.a < 0.1f) {
+			CancelInvoke ("FadeToClear");
+			FadeScreen.color = Color.clear;
+			Debug.Log (FadeScreen.color.a);
+			if (SceneManager.GetActiveScene ().name == "Lose") {
+				GameObject.Find ("Button").GetComponent<Image> ().enabled = true;
+				DestroyImmediate (GameObject.Find ("Fade"));
+				DestroyImmediate(GameObject.Find("leveltransition"));
+			}
+		}
+	}
 }
