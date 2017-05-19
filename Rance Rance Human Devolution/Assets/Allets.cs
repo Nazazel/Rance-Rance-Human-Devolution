c89f3dfaddc09412faa62437c374f1b94c5d1a72
CodiakVII <codiak759@gmail.com>
2017-05-18 21:09:23 -0700

    Animations

---
+++ Rance Rance Human Devolution/Assets/Allets.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Allets : MonoBehaviour {
+
+	private bool started;
+	private Animator allets;
+	// Use this for initialization
+	void Start () {
+		started = false;
+		allets = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!started) {
+			started = true;
+			StartCoroutine("AlletsGreat");
+		}
+	}
+
+	public IEnumerator AlletsGreat()
+	{
+		yield return new WaitForSeconds (4.0f);
+		allets.Play ("Great");
+		yield return new WaitForSeconds (1.0f);
+		allets.Play ("Idle");
+		started = false;
+	}
+}
