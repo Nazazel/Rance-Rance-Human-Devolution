94c344f378cacee38aac9b911e705d3512e40a65
InvalidCS <cjdipalma@yahoo.com>
2017-05-18 21:36:38 -0700

    More bugfixes

--- Rance Rance Human Devolution/Assets/Scripts/Notes.cs
+++ Rance Rance Human Devolution/Assets/Scripts/Notes.cs
@@ -6,41 +6,61 @@ public class Notes : MonoBehaviour
 {
     private bool hasNote = false;
     private KeyCode[] kc;
-    private GameObject lastNote, temp_note, score;
-    public GameObject n, health;
+    private GameObject lastNote, temp_note, score, health;
+    public GameObject n;
     private List<GameObject> notes = new List<GameObject>();
     private SpriteRenderer sr;
     public bool createMode;
     public Color old;
+
+    public string[] controller;
+    public Sprite xbox_sprite;
+    public Sprite key_sprite;
+
     private int note_capacity;
-    private float missBound = 0.28f, okayBound = 0.2f, goodBound = 0.11f; //Values greater than a certain bound are classified as that type of hit. Ex: outside of goodBound but within okayBound is good.
+    private float missBound = 0.29f, okayBound = 0.2f, goodBound = 0.11f; //Values greater than a certain bound are classified as that type of hit. Ex: outside of goodBound but within okayBound is good.
 
     // Use this for initialization
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         score = GameObject.FindWithTag("Score");
+        health = GameObject.Find("Health");
         old = sr.color;
         note_capacity = 0;
-        kc = new KeyCode[2];
+        kc = new KeyCode[3];
         switch (this.name)
         {
             case "Left":
                 kc[0] = KeyCode.A;
                 kc[1] = KeyCode.LeftArrow;
+                kc[2] = KeyCode.Joystick1Button2;
                 break;
             case "Right":
                 kc[0] = KeyCode.D;
                 kc[1] = KeyCode.RightArrow;
+                kc[2] = KeyCode.Joystick1Button1;
                 break;
             case "Up":
                 kc[0] = KeyCode.W;
                 kc[1] = KeyCode.UpArrow;
+                kc[2] = KeyCode.Joystick1Button3;
                 break;
             case "Down":
                 kc[0] = KeyCode.S;
                 kc[1] = KeyCode.DownArrow;
+                kc[2] = KeyCode.Joystick1Button0;
                 break;
+        }
+        controller = Input.GetJoystickNames(); //Moved this code to start() to reduce lag
+        if (controller.Length > 0 && controller[0] == "Controller (Xbox One For Windows)")
+        {
+            sr.sprite = xbox_sprite;
+        }
+
+        else
+        {
+            sr.sprite = key_sprite;
         }
     }
 
@@ -49,14 +69,14 @@ public class Notes : MonoBehaviour
     {
         if (createMode)
         {
-            if (Input.GetKeyDown(kc[0]) || Input.GetKeyDown(kc[1]))
+            if (Input.GetKeyDown(kc[0]) || Input.GetKeyDown(kc[1]) || Input.GetKeyDown(kc[2]))
             {
                 Instantiate(n, gameObject.transform.position, Quaternion.identity);
             }
         }
         else
         {
-            if (Input.GetKeyDown(kc[0]) || Input.GetKeyDown(kc[1]))
+            if (Input.GetKeyDown(kc[0]) || Input.GetKeyDown(kc[1]) || Input.GetKeyDown(kc[2]))
             {
                 StartCoroutine(pressed());
                 if (hasNote)
