fa4a352892dcba037aa412e32ac9b601e1d2cf42
TheAvianLord <angela-yixuan21@hotmail.com>
2017-05-18 20:19:33 -0700

    Credits Fixed

--- Rance Rance Human Devolution/Assets/Scripts/Credits.cs
+++ Rance Rance Human Devolution/Assets/Scripts/Credits.cs
@@ -36,12 +36,16 @@ public class Credits : MonoBehaviour
         yield return new WaitForSeconds(3.0f);
         theText.text = "Artist:\n\nVictoria Barinova";
         yield return new WaitForSeconds(3.0f);
+        theText.fontSize = 70;
         theText.text = "Programmers:\n\nUlises Perez\n\nYixuan (Angela) Li\n\nChristopher Dipalma";
         yield return new WaitForSeconds(3.0f);
+        theText.fontSize = 80;
         theText.text = "Audio/Video Gathering:\n\nNazely  Hartoonian";
         yield return new WaitForSeconds(3.0f);
-        theText.text = "Design:\n\nUlises Perez\n\n Nazely  Hartoonian";
+        theText.fontSize = 70;
+        theText.text = "Design:\n\nUlises Perez\n\n Nazely  Hartoonian\n\nYixuan (Angela) Li";
         yield return new WaitForSeconds(3.0f);
+        theText.fontSize = 80;
         theText.text = " ";
         StartCoroutine(AudioFadeOut.FadeOut(GameObject.Find("Audio Source").GetComponent<AudioSource>(), 2.5f));
         yield return new WaitForSeconds(3.0f);
